9424aea90ee756969c2946dbe54c0da74c60e001
tjones <Tyler.Jones@healthsparq.com>
2018-08-16 12:28:57 -0700

    finished problem 7

--- Project_Euler/Incomplete/Problem 71 - Ordered fractions.py
+++ Project_Euler/Incomplete/Problem 71 - Ordered fractions.py
@@ -7,6 +7,19 @@
 # It can be seen that 2/5 is the fraction immediately to the left of 3/7.
 #
 # By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, find the numerator of the fraction immediately to the left of 3/7.
+import time
+
+def trial_division(n):
+	"""Return a list of the factors for a natural number."""
+	a = []               #Prepare an empty list.
+	f = 2                #The first possible factor.
+	while f <= n:         #While n still has remaining factors...
+		if (n % f == 0):     #The remainder of n divided by f might be zero.
+			a.append(f)         #If so, it divides n. Add f to the list.
+			f += 1              #Divide that factor out of n.n += n
+		else:                #But if f is not a factor of n,
+			f += 1              #Add one to f and try again.
+	return a
 
 def find_largest_common_factor(x, y):
 	xs = trial_division(x)
@@ -14,16 +27,29 @@ def find_largest_common_factor(x, y):
 
 	common_factors = list(set(xs) & set(ys))
 	common_factors.sort()
-	print(common_factors)
 
 	if(len(common_factors) > 0):
 		return common_factors[-1]
 	else:
 		return 1
 
-fractions = []
+def sort_by_fraction_size(xs):
+	return (xs[0]/xs[1])
 
-for x in range(1,1000000):
-    for y in range (1000000, 1):
-        if(x<y):
-            if(x/y < 3/7):
+
+start_time = time.time()
+
+fractions = []
+print(3/7)
+
+for x in range(428571, 1000001):
+	for y in range (1000000, 999998, -1):
+		if(x/y < 3/7):
+			lcf = find_largest_common_factor(x, y)
+			if(((x/lcf), (y/lcf)) not in fractions):
+				print(((x/lcf), (y/lcf)))
+				fractions.append(((x/lcf), (y/lcf)))
+
+fractions.sort(key = sort_by_fraction_size)
+print(fractions)
+print("--- %s seconds ---" % (time.time() - start_time))
