fe015e0563eab92953c567be7fde5a2dfbdfa84b
tylerdj96 <tjblizz0517@gmail.com>
2018-08-21 23:37:31 -0700

    sudoku double linked list stuff

--- Project_Euler/Complete/Problem 96 - Su Doku.py
+++ Project_Euler/Complete/Problem 96 - Su Doku.py
@@ -1,5 +1,75 @@
 # By solving all fifty puzzles find the sum of the 3-digit numbers found in the top left corner of each solution grid; for example, 483 is the 3-digit number found in the top left corner of the solution grid above.
 
+class Node(object):
+
+    def __init__(self, data, prev, next):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+
+class DoubleList(object):
+    head = None
+    tail = None
+
+    def append(self, data):
+        new_node = Node(data, None, None)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            new_node.next = None
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def remove(self, node_value):
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.data == node_value:
+                # if it's not the first element
+                if current_node.prev is not None:
+                    current_node.prev.next = current_node.next
+                    current_node.next.prev = current_node.prev
+                else:
+                    # otherwise we have no prev (it's None), head is the next one, and prev becomes None
+                    self.head = current_node.next
+                    current_node.next.prev = None
+
+            current_node = current_node.next
+
+    def show(self):
+        print("Show list data:")
+        current_node = self.head
+        while current_node is not None:
+            print("&" * 25)
+            print(current_node.prev.data if hasattr(current_node.prev, "data") else None),
+            print(current_node.data),
+            print(current_node.next.data if hasattr(current_node.next, "data") else None)
+
+            current_node = current_node.next
+        print("*" * 50)
+
+
+class Spine(object):
+
+    def __init__(self):
+        
+
+d = DoubleList()
+
+d.append(5)
+d.append(6)
+d.append(50)
+d.append(30)
+
+d.show()
+
+d.remove(50)
+d.remove(5)
+
+d.show()
+
 def ingest_puzzle(file_name, puzzle_number):
     file = open(file_name, "rb")
     file.seek(8*puzzle_number+(90*(puzzle_number-1)), 1)
