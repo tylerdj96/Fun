5bc779eb50ada7232c4cf7c959cb7ed50cdb7c04
tylerdj96 <tjblizz0517@gmail.com>
2018-10-02 22:43:30 -0700

    problem 21

--- Project_Euler/Complete/Problem 21 - Amicable numbers.py
+++ Project_Euler/Complete/Problem 21 - Amicable numbers.py
@@ -4,13 +4,30 @@
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 #
 # Evaluate the sum of all the amicable numbers under 10000.
+import math
 
-def find_factors(n):
-
+def trial_division(n):
+    x = 1
     factors = []
-    x=1
-    while(x<n):
+    while(math.sqrt(n)>=x):
         if(n%x==0):
-            factors.append(n)
-
+            factors.append(x)
+            factors.append(int(n/x))
+        x += 1
+    factors.remove(n)
     return factors
+
+
+my_list = list(range(2,10000))
+amicables = []
+
+for x in my_list:
+    curr = sum(trial_division(x))
+    curr2 = sum(trial_division(curr))
+    if(x == curr2 and curr != curr2):
+        amicables.append(curr)
+        amicables.append(x)
+        my_list.remove(curr)
+        my_list.remove(x)
+print(sum(amicables))
+
