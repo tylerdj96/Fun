f39d1fbb8b915261ce983f5645691a1b5094ae54
tylerdj96 <tjblizz0517@gmail.com>
2018-12-05 00:53:04 -0800

    problem 25 ez pz

--- Project_Euler/Incomplete/Problem 24 - Lexicographic Permutations.py
+++ Project_Euler/Incomplete/Problem 24 - Lexicographic Permutations.py
@@ -10,11 +10,20 @@ nums = list(range(10)) ##declare an array to hold integers 0-9
 ## 9! = 362880, and 362880 * 3 > 1,000,000 (1,088,640) , so we know that the millionth element begins with 2, and is the 274240 element in the set of
 ## permutations that begin with 2.
 
+## 10! = 3628800
+## 9! = 362880
+## 8! = 40320
+## 7! = 5040
+## 6! = 720
+## 5! = 120
+## 4! = 24
+## 3! = 6
+## 2! = 2
+## 1 = 1
 
 
-## Let's rearrange the order of the list to have 2 at the beginning.
+## Let's rearrange the order of the list to have 2 at the beginning. 725761st permutation
 nums = [2,0,1,3,4,5,6,7,8,9]
 
 ##274240-241920 =
-nums = [2,0,6,
-
+nums = [2,0,
